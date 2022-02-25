905e80bcc0e75c859592ece13dacd2ec4edae580
markryo <dev.mryo@gmail.com>
2022-02-25 19:49:05 +0900

    Final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,20 +13,64 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepOrangeAccent,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.coffee_maker),
-                Text('WELCOME'),
-              ],
+        body: SafeArea(
+          //avoid notch
+          child: Center(
+            //Puts widget centre
+            child: SingleChildScrollView(
+              // this allows you to tilt device and avoid pixel overflow
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.coffee,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'WELCOME',
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Image.asset("images/welcome.png"),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      print('Sign in button');
+                    },
+                    child: Text('Sign In'),
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.white, width: 2.0),
+                      primary: Colors.white,
+                      minimumSize: Size(200.0, 40.0),
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {
+                      print('Register button');
+                    },
+                    child: Text('Register'),
+                    style: OutlinedButton.styleFrom(
+                        primary: Colors.deepOrangeAccent,
+                        backgroundColor: Colors.white,
+                        minimumSize: Size(200, 40)),
+                  ),
+                ],
+              ),
             ),
-            Center(
-              child: Image.asset("images/welcome.png"),
-            ),
-          ],
+          ),
         ),
       ),
     );
